778befdde5a8a1080c7a9c5d9a4513d8976f8fdb
kize <haririkaiss@gmx.de>
2018-04-19 20:55:03 +0200

    improvements

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -149,78 +149,78 @@ export class HomePage {
   }
 
   loadEventThisMonth() {
-    // this.eventList = new Array();
-    // var startDate = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
-    // var endDate = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
-    // this.calendar.listEventsInRange(startDate, endDate).then(
-    //   (msg) => {
-    //     msg.forEach(item => {
-    //       this.eventList.push(item);
-    //     });
-    //   },
-    //   (err) => {
-    //     console.log(err);
-    //   }
-    // );
+    this.eventList = new Array();
+    var startDate = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
+    var endDate = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
+    this.calendar.listEventsInRange(startDate, endDate).then(
+      (msg) => {
+        msg.forEach(item => {
+          this.eventList.push(item);
+        });
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   checkEvent(day) {
-    // var hasEvent = false;
-    // var thisDate1 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 00:00:00";
-    // var thisDate2 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 23:59:59";
-    // this.eventList.forEach(event => {
-    //   if (((event.startDate >= thisDate1) && (event.startDate <= thisDate2)) || ((event.endDate >= thisDate1) && (event.endDate <= thisDate2))) {
-    //     hasEvent = true;
-    //   }
-    // });
-    // return hasEvent;
+    var hasEvent = false;
+    var thisDate1 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 00:00:00";
+    var thisDate2 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 23:59:59";
+    this.eventList.forEach(event => {
+      if (((event.startDate >= thisDate1) && (event.startDate <= thisDate2)) || ((event.endDate >= thisDate1) && (event.endDate <= thisDate2))) {
+        hasEvent = true;
+      }
+    });
+    return hasEvent;
   }
 
   selectDate(day) {
-    // this.isSelected = false;
-    // this.selectedEvent = new Array();
-    // var thisDate1 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 00:00:00";
-    // var thisDate2 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 23:59:59";
-    // this.eventList.forEach(event => {
-    //   if (((event.startDate >= thisDate1) && (event.startDate <= thisDate2)) || ((event.endDate >= thisDate1) && (event.endDate <= thisDate2))) {
-    //     this.isSelected = true;
-    //     this.selectedEvent.push(event);
-    //   }
-    // });
+    this.isSelected = false;
+    this.selectedEvent = new Array();
+    var thisDate1 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 00:00:00";
+    var thisDate2 = this.date.getFullYear() + "-" + (this.date.getMonth() + 1) + "-" + day + " 23:59:59";
+    this.eventList.forEach(event => {
+      if (((event.startDate >= thisDate1) && (event.startDate <= thisDate2)) || ((event.endDate >= thisDate1) && (event.endDate <= thisDate2))) {
+        this.isSelected = true;
+        this.selectedEvent.push(event);
+      }
+    });
   }
 
   deleteEvent(evt) {
-    // console.log(new Date(evt.startDate.replace(/\s/, 'T')));
-    // console.log(new Date(evt.endDate.replace(/\s/, 'T')));
-    // let alert = this.alertCtrl.create({
-    //   title: 'Confirm Delete',
-    //   message: 'Are you sure want to delete this event?',
-    //   buttons: [
-    //     {
-    //       text: 'Cancel',
-    //       role: 'cancel',
-    //       handler: () => {
-    //         console.log('Cancel clicked');
-    //       }
-    //     },
-    //     {
-    //       text: 'Ok',
-    //       handler: () => {
-    //         this.calendar.deleteEvent(evt.title, evt.location, evt.notes, new Date(evt.startDate.replace(/\s/, 'T')), new Date(evt.endDate.replace(/\s/, 'T'))).then(
-    //           (msg) => {
-    //             console.log(msg);
-    //             this.loadEventThisMonth();
-    //             this.selectDate(new Date(evt.startDate.replace(/\s/, 'T')).getDate());
-    //           },
-    //           (err) => {
-    //             console.log(err);
-    //           }
-    //         )
-    //       }
-    //     }
-    //   ]
-    // });
-    // alert.present();
+    console.log(new Date(evt.startDate.replace(/\s/, 'T')));
+    console.log(new Date(evt.endDate.replace(/\s/, 'T')));
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Delete',
+      message: 'Are you sure want to delete this event?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            this.calendar.deleteEvent(evt.title, evt.location, evt.notes, new Date(evt.startDate.replace(/\s/, 'T')), new Date(evt.endDate.replace(/\s/, 'T'))).then(
+              (msg) => {
+                console.log(msg);
+                this.loadEventThisMonth();
+                this.selectDate(new Date(evt.startDate.replace(/\s/, 'T')).getDate());
+              },
+              (err) => {
+                console.log(err);
+              }
+            )
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
